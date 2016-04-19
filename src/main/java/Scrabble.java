589b3ccbed61f6d5e791a29d7cacea334b8f7749
Inthrayuth Mahaphol and Jeffrey Hackford <jeffrey@>
2016-04-19 13:39:01 -0700

    finish business logic and results page to finish the functionality

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -32,56 +32,58 @@ public class Scrabble {
 
 
   public static Integer calculateScore(String userWord) {
+    Integer totalScore = 0;
     char[] resultsCharArray = userWord.toCharArray();
 
-for
+    HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+    hm.put('a', 1);
+    hm.put('e', 1);
+    hm.put('i', 1);
+    hm.put('o', 1);
+    hm.put('u', 1);
+    hm.put('l', 1);
+    hm.put('n', 1);
+    hm.put('r', 1);
+    hm.put('s', 1);
+    hm.put('t', 1);
+    hm.put('d', 2);
+    hm.put('g', 2);
+    hm.put('b', 3);
+    hm.put('c', 3);
+    hm.put('m', 3);
+    hm.put('p', 3);
+    hm.put('f', 4);
+    hm.put('h', 4);
+    hm.put('v', 4);
+    hm.put('w', 4);
+    hm.put('y', 4);
+    hm.put('k', 5);
+    hm.put('j', 8);
+    hm.put('x', 8);
+    hm.put('q', 10);
+    hm.put('z', 10);
 
-    Hashmap hm = new Hashmap();
-    hm.put('a', 1)
-    hm.put('e', 1)
-    hm.put('i', 1)
-    hm.put('o', 1)
-    hm.put('u', 1)
-    hm.put('l', 1)
-    hm.put('n', 1)
-    hm.put('r', 1)
-    hm.put('s', 1)
-    hm.put('t', 1)
-    hm.put('d', 2)
-    hm.put('g', 2)
-    hm.put('b', 3)
-    hm.put('c', 3)
-    hm.put('m', 3)
-    hm.put('p', 3)
-    hm.put('f', 4)
-    hm.put('h', 4)
-    hm.put('v', 4)
-    hm.put('w', 4)
-    hm.put('y', 4)
-    hm.put('k', 5)
-    hm.put('j', 8)
-    hm.put('x', 8)
-    hm.put('q', 10)
-    hm.put('z', 10)
-
-      // if ( userWord.equals('a') || userWord.equals('e') || userWord.equals('i') || userWord.equals('o') || userWord.equals('u') || userWord.equals('l') || userWord.equals('n') || userWord.equals('r') || userWord.equals('s') || userWord.equals('t') ) {
-      //   return 1;
-      // } else if ( userWord.equals('d') || userWord.equals('g') ) {
-      //   return 2;
-      // } else if ( userWord.equals('b') || userWord.equals('c') || userWord.equals('m') || userWord.equals('p') ) {
-      //   return 3;
-      // } else if ( userWord.equals('f') || userWord.equals('h') || userWord.equals('v') || userWord.equals('w') || userWord.equals('y') ) {
-      //   return 4;
-      // } else if ( userWord.equals('k') ) {
-      //   return 5;
-      // } else if ( userWord.equals('x') || userWord.equals('j') ) {
-      //   return 8;
-      // } else if ( userWord.equals('z') || userWord.equals('q') ) {
-      //   return 10;
-      // } else {
-      //   return 568;
-      // }
+    for (char singleLetter : resultsCharArray) {
+      totalScore += hm.get(singleLetter);
     }
+    return totalScore;
   }
 }
-// ArrayList<Object> userWord = new ArrayList<Object>();
+
+// if ( userWord.equals('a') || userWord.equals('e') || userWord.equals('i') || userWord.equals('o') || userWord.equals('u') || userWord.equals('l') || userWord.equals('n') || userWord.equals('r') || userWord.equals('s') || userWord.equals('t') ) {
+//   return 1;
+// } else if ( userWord.equals('d') || userWord.equals('g') ) {
+//   return 2;
+// } else if ( userWord.equals('b') || userWord.equals('c') || userWord.equals('m') || userWord.equals('p') ) {
+//   return 3;
+// } else if ( userWord.equals('f') || userWord.equals('h') || userWord.equals('v') || userWord.equals('w') || userWord.equals('y') ) {
+//   return 4;
+// } else if ( userWord.equals('k') ) {
+//   return 5;
+// } else if ( userWord.equals('x') || userWord.equals('j') ) {
+//   return 8;
+// } else if ( userWord.equals('z') || userWord.equals('q') ) {
+//   return 10;
+// } else {
+//   return 568;
+// }
